1f63c0cb827e16773a0819c5371e104d3a85ab0e
txy <txy03081092@163.com>
2014-05-06 12:02:35 +0800

    3.5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,22 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Demo App')
     end
+    it "should hava the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Demp App | Home")
+    end
   end
+ 
+
 
   describe "Help page" do
     it "should hava the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+    it "should hava the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Demp App | Help")
     end
   end
 
@@ -20,6 +30,10 @@ describe "StaticPages" do
     it "should hava the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    it "should hava the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Demp App | About")
     end
   end
 end
