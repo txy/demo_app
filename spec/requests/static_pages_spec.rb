801311c16c4f0f108be229f87bc6219afaf9e369
txy <txy03081092@163.com>
2014-05-06 11:21:33 +0800

    3.4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,8 +5,21 @@ describe "StaticPages" do
   describe "Home page" do
     it "should hava the content 'Demo App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Demo App')
     end
   end
 
+  describe "Help page" do
+    it "should hava the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+    it "should hava the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
