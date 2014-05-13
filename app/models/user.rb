368bbf9f6651e2a55cc322723a384f38b072172c
txy <txy03081092@163.com>
2014-05-13 09:51:08 +0800

    5.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name         #定义属性访问器、获取和设定的方法
+  #attr_accessible :email, :name         #定义属性访问器、获取和设定的方法
   
-  has_many :microposts #user与 Microposts 1:n
+  #has_many :microposts #user与 Microposts 1:n
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ DemoApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
   resources :microposts
 
-  resources :users
+  #resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
